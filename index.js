c145439bb3cc561b036317b1580901431879f8ef
Rohan Sah <rohan2911sah@gmail.com>
2024-03-21 22:02:10 +0530

    third commit

--- index.js
+++ index.js
@@ -5,35 +5,3 @@ fetch('https://www.dev.readychatai.com/messages_json', options)
   .then(response => response.json())
   .then(response => console.log(response))
   .catch(err => console.error(err));
-
-
-// const options = {method: 'GET'};
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-	
-// 	}
-// };
-
-// https://www.dev.readychatai.com/messages_json
-
-// fetch('https://www.dev.readychatai.com/messages_json', options)
-//   .then(response => response.json())
-//   .then(response => console.log(response))
-//   .catch(err => console.error(err));
-// const data=fetch('https://www.dev.readychatai.com/messages_json');
-// console.log(data);
-
-
-//   https://www.dev.readychatai.com/messages_json
-
-
-// fetch('https://www.dev.readychatai.com/messages_json', options)
-//   .then(response => {
-//     if (!response.ok) {
-//       throw new Error('Network response was not ok');
-//     }
-//     return response.json();
-//   })
-//   .then(response => console.log(response))
-//   .catch(err => console.error('Fetch error:', err));
